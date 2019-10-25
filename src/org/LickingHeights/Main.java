058001dee3b328ac71bb311ff3482aeeac569e1d
Kyleigh <kyleigh.lisi.2020@student.lhschools.org>
2019-10-25 09:58:54 -0400

    First Commit

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int pickedNumber;
+
+
+        for(int i = 0; i <20; i++); {
+            pickedNumber = (int) (Math.random()*10)+1;
+            System.out.println(pickedNumber);
+        }
+    }
+}
