d24de15c6b424fe85187b43c1a7fa3c9e9b7d0fd
Kyleigh <kyleigh.lisi.2020@student.lhschools.org>
2019-10-31 10:05:42 -0400

    main java

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,15 +1,32 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-        int pickedNumber;
+    Scanner keyboard = new Scanner(System.in);
+
+    
 
 
-        for(int i = 0; i <20; i++); {
-            pickedNumber = (int) (Math.random()*10)+1;
-            System.out.println(pickedNumber);
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-}
